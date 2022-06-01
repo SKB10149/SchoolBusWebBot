c4a1f0b55c784e8adcb299de6296d54d6f0ec57e
SKB10149 <saito@skb-ec.co.jp>
2022-06-01 10:33:35 +0900

    Commit

--- app.py
+++ app.py
@@ -85,8 +85,12 @@ def handle_message(event):
 
     # 学校名押下
     elif event.message.text[-2:] == "学校":
+        reply_message = event.message.text
+        repMessage(event, reply_message)
         selectSchool(event, event.message.text)
     elif event.message.text[-2:] == "学園":
+        reply_message = event.message.text
+        repMessage(event, reply_message)
         selectSchool(event, event.message.text)
 
     # # コース名
